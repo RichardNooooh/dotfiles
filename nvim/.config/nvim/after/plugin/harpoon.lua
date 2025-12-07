ca692d9680838bc71ef69b903e6241bee6598afa
Richard Noh <richard.jun.noh@gmail.com>
2025-12-06 21:14:58 -0600

    feat: Add prepend hotkey

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -4,12 +4,17 @@ local harpoon = require("harpoon")
 harpoon:setup()
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = 'Harpoon [A]dd' })
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = 'Harpoon [A]dd at end' })
+vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end, { desc = 'Harpoon [A]dd at head' })
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, 
     { desc = '[E]nable Harpoon window' })
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = 'Harpoon Get 1' })
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, { desc = 'Harpoon Get 2' })
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end, { desc = 'Harpoon Get 3' })
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end, { desc = 'Harpoon Get 4' })
+-- tried the Telescope window, but I don't think I can easily move or manipulate the list
+-- with Vim motions like with the default window.
+-- TODO try make the Harpoon window look nice?
+
+vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end, { desc = 'Harpoon Get 1' })
+vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end, { desc = 'Harpoon Get 2' })
+vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end, { desc = 'Harpoon Get 3' })
+vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end, { desc = 'Harpoon Get 4' })
 
