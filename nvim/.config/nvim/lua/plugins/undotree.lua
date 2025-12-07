16cc83b5f77aeb14afcd15dab59b0a863b2ce2fa
Richard Noh <richard.jun.noh@gmail.com>
2025-12-06 22:43:22 -0600

    feat: Add Undotree and VimBeGood plugins

---
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,9 @@
+return {
+    "mbbill/undotree",
+    config = function() 
+        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle,
+                            { desc = '[U]ndo on Undotree' })
+        vim.g.undotree_DisabledFiletypes = { 'TelescopePrompt' }
+    end
+}
+
