5100cd330388b5408acfc904cbc31b8135b58ed3
Richard Noh <richard.jun.noh@gmail.com>
2025-12-06 19:35:14 -0600

    docs: Add description to netrw nvim command

--- nvim/.config/nvim/lua/custom/remap.lua
+++ nvim/.config/nvim/lua/custom/remap.lua
@@ -1,4 +1,4 @@
 -- already set in config/lazy.lua
 -- vim.g.mapleader = " "
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[P]roject [V]iew Netrw' })
 
