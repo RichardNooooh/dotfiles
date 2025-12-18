8defe0f654b94818ee95592b55b23b1ae45f0de1
RichardNooooh <richard.jun.noh@gmail.com>
2025-12-18 07:29:54 -0600

    feat: Add python LSPs and linters

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,31 @@
 
 return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "mason-org/mason.nvim",
+        "mason-org/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup()
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                -- Docker LSP + Linter
+                "docker-language-server",
+                -- "docker-compose-language-service", -- ! REQUIRES npm
+                "hadolint",
 
+                -- Lua
+                "lua_ls",
+                "luacheck",
+                "luaformatter",
 
+                -- Python
+                "ty",
+                "ruff",
+            }
+        })
+    end
 }
+
