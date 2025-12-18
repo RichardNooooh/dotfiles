9dbbc86023e38be8fb7b84dc0ea760bc7f51a11a
RichardNooooh <richard.jun.noh@gmail.com>
2025-12-18 06:24:32 -0600

    feat: Add tokyonight color theme

--- nvim/.config/nvim/lua/custom/set.lua
+++ nvim/.config/nvim/lua/custom/set.lua
@@ -37,5 +37,5 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 -- Color column?
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 
---
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,29 @@
+
+function ColorMyPencils(color)
+    vim.cmd.colorscheme(color or "tokyonight-night")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+return {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        transparent = true,
+        styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+            functions = {},
+        },
+        on_highlights = function(hl, c)
+            -- remove the highlights while keep the color
+            hl["@keyword"] = vim.tbl_extend("force", hl["@keyword"] or {}, { italic = false })
+        end
+    },
+    config = function(_, opts)
+        require("tokyonight").setup(opts) -- required since we have a config function + opts
+        ColorMyPencils()
+    end
+}
+
