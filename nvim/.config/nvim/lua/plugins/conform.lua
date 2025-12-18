09227512441739d31527b8d239dfa6785a33bc13
RichardNooooh <richard.jun.noh@gmail.com>
2025-12-18 08:02:20 -0600

    feat: Add go LSP and conform plugin

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,28 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				lua = { "stylua" },
+				go = { "gofmt" },
+				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+			},
+			formatters = {
+				["clang-format"] = {
+					prepend_args = { "-style=file", "-fallback-style=LLVM" },
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>f", function()
+			require("conform").format({ bufnr = 0 })
+		end)
+	end,
+}
