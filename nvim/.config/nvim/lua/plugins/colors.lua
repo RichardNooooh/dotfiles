7225754f979d2a7136ff7baf61db220728af7adb
RichardNooooh <richard.jun.noh@gmail.com>
2025-12-23 11:39:28 -0600

    feat: Add colors to line numbers

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -2,6 +2,9 @@ function ColorMyPencils(color)
   vim.cmd.colorscheme(color or 'tokyonight-night')
   vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
   vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = false })
+  vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = false })
+  vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F', bold = false })
 end
 
 return {
