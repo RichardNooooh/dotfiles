aa673105c51de22eb7e4a6dab90185d5c5e238b5
RichardNooooh <richard.jun.noh@gmail.com>
2025-12-23 11:39:50 -0600

    fix: Trouble plugin keybinds

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,21 +1,45 @@
 return {
   'folke/trouble.nvim',
-  config = function()
-    require('trouble').setup {
-      icons = false, -- TODO: consider turning on after getting nerd font?
-    }
+  opts = {},
+  cmd = 'Trouble',
+  keys = {
+    {
+      '<leader>td',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Trouble: [T]oggle [D]iagnostics',
+    },
+    {
+      '<leader>tdb',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = 'Trouble: [T]oggle [D]iagnostics [B]uffer',
+    },
+    {
+      '<leader>ts',
+      '<cmd>Trouble symbols toggle focus=false<cr>',
+      desc = 'Trouble: [T]oggle [S]ymbol',
+    },
+    {
+      '<leader>tl',
+      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+      desc = 'Trouble: [T]oggle [L]SP Definitions / references / ...',
+    },
+    {
+      '<leader>tL',
+      '<cmd>Trouble loclist toggle<cr>',
+      desc = 'Trouble: [T]oggle [L]ocation List',
+    },
+    {
+      '<leader>tq',
+      '<cmd>Trouble qflist toggle<cr>',
+      desc = 'Trouble: [T]oggle [Q]uickfix List',
+    },
+  },
 
-    -- TODO: Test Trouble plugin ("No mode specified" error)
-    vim.keymap.set('n', '<leader>tt', function()
-      require('trouble').toggle()
-    end, { desc = '[T]rouble [T]oggle' })
-
-    vim.keymap.set('n', '<leader>tn', function()
-      require('trouble').next { skip_groups = true, jump = true }
-    end, { desc = '[T]rouble [N]ext' })
-
-    vim.keymap.set('n', '<leader>tp', function()
-      require('trouble').previous { skip_groups = true, jump = true }
-    end, { desc = '[T]rouble [P]revious' })
-  end,
+  -- vim.keymap.set('<leader>tn', function()
+  --   require('trouble').next { skip_groups = true, jump = true }
+  -- end, { desc = '[T]rouble [N]ext' })
+  --
+  -- vim.keymap.set('<leader>tp', function()
+  --   require('trouble').previous { skip_groups = true, jump = true }
+  -- end, { desc = '[T]rouble [P]revious' })
 }
