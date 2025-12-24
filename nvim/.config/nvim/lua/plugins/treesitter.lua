9ec2a7ad597afbc0fb9d5b2b11293e44ce9b05eb
RichardNooooh <richard.jun.noh@gmail.com>
2025-12-24 10:15:55 -0600

    feat: Ensure golang treesitter installation

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -9,6 +10,7 @@ return {
       'c',
       'diff',
       'python',
+      'go',
       'lua',
       'luadoc',
       'markdown',
@@ -24,9 +26,9 @@ return {
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby', 'python' },
+      additional_vim_regex_highlighting = { 'python' },
     },
-    indent = { enable = true, disable = { 'ruby', 'python' } },
+    indent = { enable = true, disable = { 'python' } },
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
